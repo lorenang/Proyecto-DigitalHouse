fbbcb22ec5d5f352190ae12cba81b1f6886a22a8
ositoCruz <mjj_pc@hotmail.com>
2024-01-19 10:04:09 -0300

    actualizacion de rama

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
 const express = require('express');
 const app = express();
+const path= require("path");
+const methodOverride = require('method-override')
+const router=require('./routers/main');
+const productsRouter = require('./routers/products');
 
-const mainRoutes = require('./routes/mainRoutes');
+const PORT= process.env.PORT || 3001;
 
-app.use(express.static('./public'));
-// Configurar EJS como motor de plantillas
-app.set('view engine', 'ejs');
 
-app.use('/', mainRoutes);
+app.use(express.static(path.join('./public')));
+app.use(methodOverride('_method'));
+app.use("/", router);
+app.use('/products', productsRouter);
+app.use('/login', router);
+app.use('/register', router);
+app.use('/about', router);
+app.use('/carritoDeCompras', router);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`click para dirigirte al sitio: http://localhost:${PORT}/`);
+
+
+
+
+
+
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, '/views'));
+
+app.use(express.json());
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor andando en puerto: http://localhost:${PORT}`);
 });
 
-// app.get('/', (req,res)=>{
-//     res.sendFile(__dirname + '/views/index.html');
-// });
-
-
-// app.get('/login', (req,res)=>{
-//     res.sendFile(__dirname + '/views/login.html');
-// });
-
-// app.get('/carrito', (req,res)=>{
-//     res.sendFile(__dirname + '/views/carritoDeCompras.html')});
-// app.get('/register', (req,res)=>{
-//     res.sendFile(__dirname + '/views/register.html');
-// });
-// app.get('/details', (req,res)=>{
-//     res.sendFile(__dirname + '/views/productDetail.html');
-// });
+/*app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+app.get('/carrito', (req,res)=>{
+    res.sendFile(__dirname + '/views/carritoDeCompras.html')});
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.get('/details', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});*/
